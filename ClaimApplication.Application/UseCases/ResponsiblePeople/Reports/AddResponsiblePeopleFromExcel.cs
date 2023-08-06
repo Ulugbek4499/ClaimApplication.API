00a9abb082333cc9b4ec954f20755da6521e4956
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-08-06 13:54:46 +0500

    save

--- ClaimApplication.Application/UseCases/ResponsiblePeople/Reports/AddResponsiblePeopleFromExcel.cs
+++ ClaimApplication.Application/UseCases/ResponsiblePeople/Reports/AddResponsiblePeopleFromExcel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using AutoMapper;
+﻿using AutoMapper;
 using ClaimApplication.Application.Commons.Interfaces;
 using ClaimApplication.Application.UseCases.ResponsiblePeople.Response;
 using ClaimApplication.Domain.Entities;
