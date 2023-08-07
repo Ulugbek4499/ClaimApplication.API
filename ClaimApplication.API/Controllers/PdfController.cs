114fbaed1b9fb4c77a12d8bb5f62a0b42015081d
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-08-07 17:15:45 +0500

    save

--- ClaimApplication.API/Controllers/PdfController.cs
+++ ClaimApplication.API/Controllers/PdfController.cs
@@ -24,27 +24,21 @@ namespace ClaimApplication.API.Controllers
         {
             try
             {
-                // Get the application data from the database
                 var application = await _dbContext.Applications.FindAsync(applicationId);
                 if (application == null)
                     return NotFound();
 
-                // Create a dynamic model from the Application object
                 dynamic model = new ExpandoObject();
                 model.Inn = application.Inn;
                 model.NameOfBussiness = application.NameOfBussiness;
                 model.AppealNumber = application.AppealNumber;
                 model.AppealDate = application.AppealDate;
-                // Add other properties as needed...
 
-                // Read the Razor template file
                 string templatePath = "ClaimApplicationTemplate.cshtml";
                 string htmlContent = await RenderRazorTemplate(templatePath, model);
 
-                // Generate the PDF from the HTML content
                 byte[] pdfBytes = GeneratePdfFromHtml(htmlContent);
 
-                // Set the content type and headers for the response
                 var response = new HttpResponseMessage();
                 response.Content = new ByteArrayContent(pdfBytes);
                 response.Content.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("attachment");
@@ -56,21 +50,18 @@ namespace ClaimApplication.API.Controllers
             }
             catch (Exception ex)
             {
-                // Handle any exceptions that occur during PDF generation
                 return InternalServerError(ex);
             }
         }
 
         private async Task<string> RenderRazorTemplate(string templatePath, dynamic model)
         {
-            // Read the Razor template file from wwwroot
             string templateContent = File.ReadAllText(System.IO.Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", templatePath));
 
             var engine = new RazorLightEngineBuilder()
                 .UseMemoryCachingProvider()
                 .Build();
 
-            // Use dynamic as the model type for the Razor template
             string renderedContent = await engine.CompileRenderStringAsync(templateContent, templatePath, model);
 
             return renderedContent;
@@ -102,6 +93,5 @@ namespace ClaimApplication.API.Controllers
         public string NameOfBussiness { get; set; }
         public int AppealNumber { get; set; }
         public DateTime AppealDate { get; set; }
-        // Add other properties as needed...
     }
 }
