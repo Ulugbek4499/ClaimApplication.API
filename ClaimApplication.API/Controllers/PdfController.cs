b4317327b830a4797a621275718443562c70c5b5
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-08-08 00:01:39 +0500

    save

--- ClaimApplication.API/Controllers/PdfController.cs
+++ ClaimApplication.API/Controllers/PdfController.cs
@@ -1,97 +1,32 @@
-﻿using System.Dynamic;
-using System.Web.Http;
-using ClaimApplication.Application.Commons.Interfaces;
-using iTextSharp.text;
-using iTextSharp.text.pdf;
-using iTextSharp.tool.xml;
-using RazorLight;
+﻿using System.Web.Http;
+using ClaimApplication.Application.UseCases.Applications.Reports;
+using Microsoft.AspNetCore.Mvc;
+using HttpGetAttribute = Microsoft.AspNetCore.Mvc.HttpGetAttribute;
+using HttpPostAttribute = Microsoft.AspNetCore.Mvc.HttpPostAttribute;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
 
-namespace ClaimApplication.API.Controllers
+namespace ClaimApplication.API.Controllers;
+
+
+[Route("api/[controller]")]
+public class PdfController : BaseApiController
 {
-    [Route("api/[controller]")]
-    public class PdfController : ApiController
+    [HttpPost("[action]")]
+    public IActionResult GetHtmlTemplate()
     {
-        private readonly IApplicationDbContext _dbContext;
+        var res = GetHtmlTemplate();
 
-        public PdfController(IApplicationDbContext dbContext)
-        {
-            _dbContext = dbContext;
-        }
-
-        [System.Web.Http.HttpGet]
-        [System.Web.Http.Route("api/pdf/{applicationId}")]
-        public async Task<IHttpActionResult> GeneratePdf(int applicationId)
-        {
-            try
-            {
-                var application = await _dbContext.Applications.FindAsync(applicationId);
-                if (application == null)
-                    return NotFound();
-
-                dynamic model = new ExpandoObject();
-                model.Inn = application.Inn;
-                model.NameOfBussiness = application.NameOfBussiness;
-                model.AppealNumber = application.AppealNumber;
-                model.AppealDate = application.AppealDate;
-
-                string templatePath = "ClaimApplicationTemplate.cshtml";
-                string htmlContent = await RenderRazorTemplate(templatePath, model);
-
-                byte[] pdfBytes = GeneratePdfFromHtml(htmlContent);
-
-                var response = new HttpResponseMessage();
-                response.Content = new ByteArrayContent(pdfBytes);
-                response.Content.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("attachment");
-                response.Content.Headers.ContentDisposition.FileName = "ClaimApplication.pdf";
-                response.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/pdf");
-                response.StatusCode = System.Net.HttpStatusCode.OK;
-
-                return ResponseMessage(response);
-            }
-            catch (Exception ex)
-            {
-                return InternalServerError(ex);
-            }
-        }
-
-        private async Task<string> RenderRazorTemplate(string templatePath, dynamic model)
-        {
-            string templateContent = File.ReadAllText(System.IO.Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", templatePath));
-
-            var engine = new RazorLightEngineBuilder()
-                .UseMemoryCachingProvider()
-                .Build();
-
-            string renderedContent = await engine.CompileRenderStringAsync(templateContent, templatePath, model);
-
-            return renderedContent;
-        }
-
-        private byte[] GeneratePdfFromHtml(string htmlContent)
-        {
-            using (MemoryStream memoryStream = new MemoryStream())
-            {
-                Document document = new Document();
-                PdfWriter writer = PdfWriter.GetInstance(document, memoryStream);
-
-                document.Open();
-
-                using (StringReader stringReader = new StringReader(htmlContent))
-                {
-                    XMLWorkerHelper.GetInstance().ParseXHtml(writer, document, stringReader);
-                }
-
-                document.Close();
-
-                return memoryStream.ToArray();
-            }
-        }
+        return Ok(res);
     }
-    public class ApplicationViewModel
+
+    [HttpGet("[action]")]
+    public async Task<ActionResult<FormFile>> GetPdfTemplate(int id)
     {
-        public string Inn { get; set; }
-        public string NameOfBussiness { get; set; }
-        public int AppealNumber { get; set; }
-        public DateTime AppealDate { get; set; }
+        var result = await _mediator.Send(new GetApplicationPDFQuery(id));
+
+        return File(result.FileContents, result.Options, result.FileName);
     }
 }
+
+
+
--- ClaimApplication.Application/UseCases/Applications/PDF/GetApplicationByIdPDF.cs
+++ ClaimApplication.Application/UseCases/Applications/PDF/GetApplicationByIdPDF.cs
@@ -1,150 +1,231 @@
 ﻿using AutoMapper;
 using ClaimApplication.Application.Commons.Exceptions;
 using ClaimApplication.Application.Commons.Interfaces;
+using ClaimApplication.Application.UseCases.Applications.PDF;
 using ClaimApplication.Application.UseCases.Applications.Response;
 using ClaimApplication.Application.UseCases.ResponsiblePeople.Reports;
-using iTextSharp.text;
-using iTextSharp.text.pdf;
+using iText.Html2pdf.Resolver.Font;
+using iText.Html2pdf;
+using iText.Kernel.Pdf;
+using iText.Layout.Element;
 using MediatR;
+using iText.Kernel.Geom;
+using iText.Layout;
 
 namespace ClaimApplication.Application.UseCases.Applications.Reports
 {
-    public record GetApplicationByIdPDFQuery(int Id) : IRequest<PDFExportResponse>;
+    public record GetApplicationPDFQuery(int Id) : IRequest<PDFExportResponse>;
 
-    public class GetApplicationByIdPDFQueryHandler : IRequestHandler<GetApplicationByIdPDFQuery, PDFExportResponse>
+    public class GetApplicationPDFQueryHandler : IRequestHandler<GetApplicationPDFQuery, PDFExportResponse>
     {
         private readonly IApplicationDbContext _dbContext;
         private readonly IMapper _mapper;
 
-        public GetApplicationByIdPDFQueryHandler(IApplicationDbContext dbContext, IMapper mapper)
+        public GetApplicationPDFQueryHandler(IApplicationDbContext dbContext, IMapper mapper)
         {
             _dbContext = dbContext;
             _mapper = mapper;
         }
 
-        public async Task<PDFExportResponse> Handle(GetApplicationByIdPDFQuery request, CancellationToken cancellationToken)
+        public async Task<PDFExportResponse> Handle(GetApplicationPDFQuery request, CancellationToken cancellationToken)
         {
-            var application = FilterIfApplicationExists(request.Id);
+            byte[] ByteArray = GetPdfTemplate(request.Id);
+
+            return new PDFExportResponse(ByteArray, "application/pdf", "ClaimApplication.html");
+        }
+
+        public byte[] GetPdfTemplate(int applicationId)
+        {
+            var application = FilterIfApplicationExists(applicationId);
 
             var result = _mapper.Map<ApplicationResponse>(application);
 
-            using (MemoryStream ms = new MemoryStream())
-            {
-                Document document = new Document();
-                document.SetMargins(20, 20, 40, 40);
-                document.SetPageSize(PageSize.A4);
-
-                PdfWriter writer = PdfWriter.GetInstance(document, ms);
-
-                HeaderFooterHelper headerFooter = new HeaderFooterHelper();
-                writer.PageEvent = headerFooter;
-
-                document.Open();
-
-                PdfPTable table = new PdfPTable(2); 
-
-                table.SetWidths(new float[] { 2f, 3f }); 
-
-                table.SpacingBefore = 10;
-                table.SpacingAfter = 10;
-
-                table.AddCell("ID");
-                table.AddCell(result.Id.ToString());
-                table.CompleteRow();
-
-                table.AddCell("Inn");
-                table.AddCell(result.Inn);
-                table.CompleteRow();
-
-                table.AddCell("Name Of Bussiness");
-                table.AddCell(result.NameOfBussiness);
-                table.CompleteRow();
-
-                table.AddCell("Appeal Number");
-                table.AddCell(result.AppealNumber.ToString());
-                table.CompleteRow();
-
-                table.AddCell("Appeal Date");
-                table.AddCell(result.AppealDate.ToString());
-                table.CompleteRow();
-
-                table.AddCell("Membership Agreement Number");
-                table.AddCell(result.MembershipAgreementNumber);
-                table.CompleteRow();
-
-                table.AddCell("Membership Agreement Date");
-                table.AddCell(result.MembershipAgreementDate.ToString());
-                table.CompleteRow();
-
-                table.AddCell("Certificate Number");
-                table.AddCell(result.CertificateNumber);
-                table.CompleteRow();
-
-                table.AddCell("Certificate Given Date");
-                table.AddCell(result.CertificateGivenDate.ToString());
-                table.CompleteRow();
-
-                table.AddCell("Previous Appeal");
-                table.AddCell(result.PreviousAppeal);
-                table.CompleteRow();
-
-                table.AddCell("Appeal Text");
-                table.AddCell(result.AppealText);
-                table.CompleteRow();
-
-                table.AddCell("Total Claim Amount");
-                table.AddCell(result.TotalClaimAmount.ToString());
-                table.CompleteRow();
-
-                table.AddCell("Main Debt");
-                table.AddCell(result.MainDebt.ToString());
-                table.CompleteRow();
-
-                table.AddCell("Calculated Late Charges");
-                table.AddCell(result.CalculatedLateCharges.ToString());
-                table.CompleteRow();
-
-                table.AddCell("Amount Of Fine");
-                table.AddCell(result.AmountOfFine.ToString());
-                table.CompleteRow();
-
-                table.AddCell("Percentage");
-                table.AddCell(result.Percentage.ToString());
-                table.CompleteRow();
-
-                table.AddCell("Appeal Predmet Id");
-                table.AddCell(result.AppealPredmetId.ToString());
-                table.CompleteRow();
-
-                table.AddCell("Appeal Type Id");
-                table.AddCell(result.AppealTypeId.ToString());
-                table.CompleteRow();
-
-                table.AddCell("Created By");
-                table.AddCell(result.CreatedBy);
-                table.CompleteRow();
-
-                table.AddCell("Created Date");
-                table.AddCell(result.CreatedDate.ToString());
-                table.CompleteRow();
-
-                table.AddCell("Modify By");
-                table.AddCell(result.ModifyBy);
-                table.CompleteRow();
-
-                table.AddCell("Modify Date");
-                table.AddCell(result.ModifyDate.ToString());
-                table.CompleteRow();
-
-                document.Add(table);
-                document.Close();
-
-                return await Task.FromResult(new PDFExportResponse(ms.ToArray(), "application/pdf", $"{request.Id}_Application.pdf"));
-            }
+            return ReturnReadyPdf(GetHtmlTemplate(result));
         }
 
         private Domain.Entities.Application FilterIfApplicationExists(int id)
-            => _dbContext.Applications.Find(id) ?? throw new NotFoundException("There is no Application with this ID.");
-    }
+               => _dbContext.Applications.Find(id) ?? throw new NotFoundException("There is no Application with this ID.");
 
+        public string GetHtmlTemplate(ApplicationResponse application)
+        {
+            var inn = application.Inn;
+            var nameOfBussiness = application.NameOfBussiness;
+            var appealNumber = application.AppealNumber;
+            var appealDate = application.AppealDate;
+            var membershipAgreementNumber = application.MembershipAgreementNumber;
+            var membershipAgreementDate = application.MembershipAgreementDate;
+            var certificateNumber = application.CertificateNumber;
+            var certificateGivenDate = application.CertificateGivenDate;
+            var previousAppeal = application.PreviousAppeal;
+            var appealText = application.AppealText;
+            var totalClaimAmount = application.TotalClaimAmount;
+            var mainDebt = application.MainDebt;
+            var calculatedLateCharges = application.CalculatedLateCharges;
+            var amountOfFine = application.AmountOfFine;
+            var percentage = application.Percentage;
+            var appealPredmetId = application.AppealPredmetId;
+            var appealTypeId = application.AppealTypeId;
+
+            Dictionary<string, string> data
+                = new Dictionary<string, string>()
+                {
+               {"${INN}", inn},
+               {"${ContractorRegion}",nameOfBussiness},
+               {"${ContractorDistrict}",appealNumber.ToString()},
+               {"${ContractorName}",appealDate.ToString()},
+               {"${ContractorDirector}",membershipAgreementNumber},
+               {"${ContractorInn}",membershipAgreementDate.ToString()},
+               {"${ContractorBankCode}",certificateNumber},
+               {"${ContractorBankName}",certificateGivenDate.ToString()},
+               {"${ContractorSettlementAccount}",previousAppeal},
+               {"${OrgRegion}",appealText},
+               {"${OrgDistrict}",totalClaimAmount.ToString()},
+               {"${OrgAdress}",mainDebt.ToString()},
+               {"${OrgBankSettlementAccount}",calculatedLateCharges.ToString()},
+               {"${OrgBankCode}",amountOfFine.ToString()},
+               {"${OrgBankName}",percentage.ToString()},
+               {"${OrgInn}",appealPredmetId.ToString()},
+               {"${ContractorPosition1}",appealTypeId.ToString()}
+                };
+
+            string fileName = "ClaimApplication.html";
+
+
+            return ReturnReadyHtmlAsString(filename: fileName,
+                                                      data: data);
+        }
+
+        public byte[] ReturnReadyPdf(string htmlString)
+        {
+            var workStream = new MemoryStream();
+
+            using (var pdfWriter = new PdfWriter(workStream))
+            {
+                pdfWriter.SetCloseStream(false);
+
+                var pdfDocument = new PdfDocument(pdfWriter);
+
+                var documentConvertProperties = new ConverterProperties();
+
+                using (var document = HtmlConverter.ConvertToDocument(htmlString, pdfDocument, documentConvertProperties))
+                {
+                    document.SetBottomMargin(0);
+                    document.SetTopMargin(0);
+                    document.Flush();
+                }
+            }
+
+            workStream.Position = 0;
+            return workStream.ToArray();
+        }
+
+        public string ReturnReadyHtmlAsString(string filename, Dictionary<string, string> data)
+        {
+            var html = GetFileFromStorage(filename);
+
+            string htmlStream = html.ReadAsString();
+
+            if (data != null)
+                foreach (var (key, value) in data)
+                    if (htmlStream.Contains(key))
+                        htmlStream = htmlStream.Replace(key, value);
+                    else
+                        htmlStream = htmlStream.Replace(key, "");
+
+            return htmlStream;
+        }
+
+        private FileInfo GetFileFromStorage(string filename)
+        {
+            string filePath = "D:\\PDP\\COMPANIES\\ClaimApplication.API\\ClaimApplication.API\\wwwroot\\html\\ClaimApplication.html"; //System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "wwwroot", "html", filename);
+            FileInfo fileInfo = new FileInfo(filePath);
+
+            return fileInfo;
+        }
+
+
+
+
+
+        public static string HtmlToPdf(string htmlFilePath, bool isLandscapeOrientation = false)
+        {
+            string tempPath = "html/";
+            var fileInfo = new FileInfo(htmlFilePath);
+            string pdfFileName = $"{tempPath}{Guid.NewGuid()}.pdf";
+
+            using (var htmlStream = File.Open(htmlFilePath, FileMode.Open, FileAccess.Read))
+            {
+                var writerProperties = new WriterProperties();
+
+                using (var pdfWriter = new PdfWriter(pdfFileName, writerProperties))
+                {
+                    var pdfDocument = new PdfDocument(pdfWriter);
+                    var pageSize = PageSize.A4;
+
+                    if (isLandscapeOrientation)
+                        pageSize = pageSize.Rotate();
+
+                    var documentConvertProperties = new ConverterProperties();
+
+                    DefaultFontProvider fontProvider = new DefaultFontProvider(true, true, true, "Times New Roman");
+
+                    string fontDir = System.IO.Path.Combine("appdata", "staticfiles", "fonts");
+
+                    if (Directory.Exists(fontDir))
+                        fontProvider.AddDirectory(fontDir);
+
+                    documentConvertProperties.SetFontProvider(fontProvider);
+
+                    pdfDocument.SetDefaultPageSize(pageSize);
+
+                    #region Convert to html
+                    IList<IElement> elements = HtmlConverter.ConvertToElements(htmlStream, documentConvertProperties);
+                    pdfDocument.SetTagged();
+
+                    Document doc = new Document(pdfDocument, pageSize);
+
+                    foreach (IElement element in elements)
+                        doc.Add((IBlockElement)element);
+                    doc.Close();
+                    #endregion
+                }
+            }
+
+            return pdfFileName;
+        }
+
+        public static void AddTextToPdf(Stream source, Stream destination, PdfText text)
+        {
+            AddTextToPdf(
+                source,
+                destination,
+                text == null ? null : new PdfText[] { text });
+        }
+
+        public static void AddTextToPdf(Stream source, Stream destination, IEnumerable<PdfText> texts)
+        {
+            using (var pdfReader = new PdfReader(source))
+            {
+                using (var ms = new MemoryStream())
+                {
+                    using (var pdfWriter = new PdfWriter(ms))
+                    {
+                        var pdfDocument = new PdfDocument(pdfReader, pdfWriter);
+                        var pageSize = PageSize.A4;
+                        pdfDocument.SetDefaultPageSize(pageSize);
+                        pdfDocument.SetTagged();
+                        Document doc = new Document(pdfDocument);
+
+                        if (texts != null)
+                            foreach (var text in texts)
+                                doc.ShowTextAligned(text.Value, text.X, text.Y, text.Page, text.Alignment, text.VerticalAlignment, text.RadAngle);
+
+                        doc.Close();
+                        var bytes = ms.ToArray();
+                        destination.Write(bytes, 0, bytes.Length);
+                    }
+                }
+            }
+        }
+    }
 }
