e62402e97c3e881af161aaa24cee5f04d57a5dd2
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-08-08 17:48:23 +0500

    save

---
+++ ClaimApplication.API/Controllers/WordDocController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ClaimApplication.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class WordDocController : BaseApiController
+{
+    [HttpPost("[action]")]
+    public async Task<IActionResult> WordToBase64(IFormFile wordFile)
+    {
+        if (wordFile == null || wordFile.Length <= 0)
+        {
+            return BadRequest("No file uploaded or the file is empty.");
+        }
+
+        using (var memoryStream = new MemoryStream())
+        {
+            await wordFile.CopyToAsync(memoryStream);
+            byte[] fileBytes = memoryStream.ToArray();
+            string base64String = Convert.ToBase64String(fileBytes);
+
+            return Ok(base64String);
+        }
+    }
+
+    [HttpPost("[action]")]
+    public IActionResult Base64ToWordDocument([FromBody] string base64Data)
+    {
+        if (string.IsNullOrWhiteSpace(base64Data))
+        {
+            return BadRequest("Base64 data is missing.");
+        }
+
+        try
+        {
+            byte[] documentBytes = Convert.FromBase64String(base64Data);
+
+            string contentType = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+
+            string fileName = "document.docx";
+
+            return File(documentBytes, contentType, fileName);
+        }
+        catch (FormatException)
+        {
+            return BadRequest("Invalid Base64 data.");
+        }
+    }
+
+    
+}
+
+
---
+++ ClaimApplication.Application/UseCases/WordBase64/AddApplicationsFromExcel.cs
@@ -0,0 +1,76 @@
+﻿using AutoMapper;
+using ClaimApplication.Application.Commons.Interfaces;
+using ClaimApplication.Application.UseCases.Applications.Response;
+using ClosedXML.Excel;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+
+namespace ClaimApplication.Application.UseCases.Applications.Reports
+{
+    public record ConvertWordToBase64(IFormFile DocFile) : IRequest<string>;
+
+    public class ConvertWordToBase64Handler : IRequestHandler<ConvertWordToBase64, string>
+    {
+
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public ConvertWordToBase64Handler(IApplicationDbContext context, IMapper mapper)
+        {
+
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<string> Handle(ConvertWordToBase64 request, CancellationToken cancellationToken)
+        {
+            if (request.DocFile == null || request.DocFile.Length == 0)
+                throw new ArgumentNullException("File", "file is empty or null");
+
+            var file = request.DocFile;
+
+            List<Domain.Entities.Application> result = new();
+            using (var ms = new MemoryStream())
+            {
+                await file.CopyToAsync(ms, cancellationToken);
+                using (var wb = new XLWorkbook(ms))
+                {
+                    var sheet1 = wb.Worksheet(1);
+                    int startRow = 2;
+                    for (int row = startRow; row <= sheet1.LastRowUsed().RowNumber(); row++)
+                    {
+                        var Application = new Domain.Entities.Application()
+                        {
+                            Inn = sheet1.Cell(row, 1).GetString(),
+                            NameOfBussiness = sheet1.Cell(row, 2).GetString(),
+                            AppealNumber = int.Parse(sheet1.Cell(row, 3).GetString()),
+                            AppealDate = DateTime.Parse(sheet1.Cell(row, 4).GetString()),
+                            MembershipAgreementNumber = sheet1.Cell(row, 5).GetString(),
+                            MembershipAgreementDate = DateTime.Parse(sheet1.Cell(row, 6).GetString()),
+                            CertificateNumber = sheet1.Cell(row, 7).GetString(),
+                            CertificateGivenDate = DateTime.Parse(sheet1.Cell(row, 8).GetString()),
+                            PreviousAppeal = sheet1.Cell(row, 9).GetString(),
+                            AppealText = sheet1.Cell(row, 10).GetString(),
+                            TotalClaimAmount = decimal.Parse(sheet1.Cell(row, 11).GetString()),
+                            MainDebt = decimal.Parse(sheet1.Cell(row, 12).GetString()),
+                            CalculatedLateCharges = decimal.Parse(sheet1.Cell(row, 13).GetString()),
+                            AmountOfFine = decimal.Parse(sheet1.Cell(row, 14).GetString()),
+                            Percentage = decimal.Parse(sheet1.Cell(row, 15).GetString()),
+                            AppealPredmetId = int.Parse(sheet1.Cell(row, 16).GetString()),
+                            AppealTypeId = int.Parse(sheet1.Cell(row, 17).GetString())
+                        };
+
+                        result.Add(Application);
+                    }
+                }
+            }
+
+            await _context.Applications.AddRangeAsync(result);
+            await _context.SaveChangesAsync();
+            //return _mapper.Map<List<ApplicationResponse>>(result);
+
+            return null;
+        
+        }
+    }
+}
