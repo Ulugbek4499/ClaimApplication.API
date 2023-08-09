d7aa230eb67207261fb0e4c56e1e60513dfdcce2
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-08-09 15:14:27 +0500

    save

--- ClaimApplication.Application/UseCases/MembershipApplications/Queries/GetMembershipApplicationById/GetMembershipByIdQuery.cs
+++ ClaimApplication.Application/UseCases/MembershipApplications/Queries/GetMembershipApplicationById/GetMembershipByIdQuery.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using ClaimApplication.Application.Commons.Exceptions;
+using ClaimApplication.Application.Commons.Interfaces;
+using ClaimApplication.Application.UseCases.Applications.Response;
+using ClaimApplication.Domain.Memberships;
+using MediatR;
 
 namespace ClaimApplication.Application.UseCases.Memberships.Queries.GetMembershipById
 {
-    internal class GetMembershipByIdQuery
+    public record GetMembershipApplicationByIdQuery(int Id) : IRequest<MembershipApplicationResponse>;
+
+    public class GetMembershipApplicationByIdQueryHandler : IRequestHandler<GetMembershipApplicationByIdQuery, MembershipApplicationResponse>
     {
+        IApplicationDbContext _dbContext;
+        IMapper _mapper;
+
+        public GetMembershipApplicationByIdQueryHandler(IApplicationDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+
+        public async Task<MembershipApplicationResponse> Handle(GetMembershipApplicationByIdQuery request, CancellationToken cancellationToken)
+        {
+            var membershipApplication = FilterIfMembershipApplicationExsists(request.Id);
+
+            var result = _mapper.Map<MembershipApplicationResponse>(membershipApplication);
+            return await Task.FromResult(result);
+        }
+
+        private MembershipApplication FilterIfMembershipApplicationExsists(int id)
+            => _dbContext.MembershipApplications
+                .Find(id)
+                     ?? throw new NotFoundException(
+                            " There is no MembershipApplication with this Id. ");
     }
 }
--- ClaimApplication.Application/UseCases/MembershipApplications/Response/MembershipApplicationResponse.cs
+++ ClaimApplication.Application/UseCases/MembershipApplications/Response/MembershipApplicationResponse.cs
@@ -1,14 +1,53 @@
 ﻿using ClaimApplication.Application.UseCases.ResponsiblePeople.Response;
+using ClaimApplication.Domain.Memberships;
 
 namespace ClaimApplication.Application.UseCases.Applications.Response
 {
     public class MembershipApplicationResponse
     {
         public int Id { get; set; }
-  
-
-
-
+        public string? NameOfBusiness { get; set; }
+        public string? FullNameOfManager { get; set; }
+        public Gender? Gender { get; set; }
+        public string? Address { get; set; }
+        public string? PostIndex { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? MobileNumberFirst { get; set; }
+        public string? MobileNumberSecond { get; set; }
+        public string? Fax { get; set; }
+        public string? MobileNumberExtra { get; set; }
+        public string? EmailFirst { get; set; }
+        public string? EmailSecond { get; set; }
+        public string? WebSite { get; set; }
+        public string? SkypeProfile { get; set; }
+        public string? FaceBookProfile { get; set; }
+        public string? TelegramProfile { get; set; }
+        public string? ExtraProfile { get; set; }
+        public DateTime? BussinessRegesteredDate { get; set; }
+        public string? RegistrationNumber { get; set; }
+        public bool? HasCopyOfRegistrationCerteficate { get; set; }
+        public string? Inn { get; set; }
+        public string? OKED { get; set; }
+        public int? NumberOfEmployees { get; set; }
+        public string? NameOfBank { get; set; }
+        public string? CodeOfBank { get; set; }
+        public string? BankAccount { get; set; }
+        public decimal? AnnualTurnoverOfEnterprise { get; set; }
+        public decimal? AnnualPaidTax { get; set; }
+        public decimal? AnnualExportAmount { get; set; }
+        public decimal? AnnualImportAmount { get; set; }
+        public decimal? AnnualProductionAmount { get; set; }
+        public string? BrandName { get; set; }
+        public string? BithDateOfManager { get; set; }
+        public string? SeriesOfPassport { get; set; }
+        public string? NumberOfPassport { get; set; }
+        public string? PassportGivenFrom { get; set; }
+        public string? Nationality { get; set; }
+        public string? EducationDegree { get; set; }
+        public string? ExtraInformation { get; set; }
+        public int? ForeignLanguageId { get; set; }
+        public int? MainActivityId { get; set; }
+        public int? BussinessCategoryId { get; set; }
         public string? CreatedBy { get; set; }
         public DateTime CreatedDate { get; set; }
         public string? ModifyBy { get; set; }
